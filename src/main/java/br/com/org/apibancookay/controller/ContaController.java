caa4de2432fecdeaab328cf77b48bbfddc7b9b5a
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-02-26 18:37:43 -0300

    Comitado por Abner

--- src/main/java/br/com/org/apibancookay/controller/ContaController.java
+++ src/main/java/br/com/org/apibancookay/controller/ContaController.java
@@ -1,22 +1,14 @@
 package br.com.org.apibancookay.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.org.apibancookay.dto.ContaDto;
@@ -79,19 +71,6 @@ public class ContaController {
 		Conta contaAlterada = contaInterface.alterar(resultado);
 		BeanUtils.copyProperties(contaAlterada, contaDto);
 		return ResponseEntity.ok(contaDto);
-	}
-
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, String> MethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		Map<String, String> errors = new HashMap<>();
-		e.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-
-		return errors;
 	}
 
 }
